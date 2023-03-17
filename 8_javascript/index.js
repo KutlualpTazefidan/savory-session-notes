6f90ef500f253c3a2c9fb37993c3b79dbf3cc487
= <=>
2023-03-17 10:55:33 +0100

    math

---
+++ 8_javascript/index.js
@@ -0,0 +1,28 @@
+var oldUse = "Don't use it";
+let newVariableChange = "I can change me";
+const myValueIsConstant = "You are stuck with me";
+console.log("oldUse:", oldUse);
+console.log("let:", newVariableChange);
+console.log("const:", oldUse);
+newVariableChange = 10;
+console.log("newVar", newVariableChange);
+//myValueIsConstant = 10;
+//console.log("whathappen", myValueIsConstant); // error
+let myBoolean = false;
+let myString = "Hey";
+let myNumber = 2;
+let myBigNumber = 2000n;
+let myNull = null;
+let myArray = ["Element 1", 2, false];
+let myObject = {
+  cohortName: "Savory",
+  amount: 14,
+};
+
+// let allVars = Object.keys(window);
+// console.log(allVars);
+
+let numbervalc = 10;
+let asd = 4;
+let das = 12 ^^ 2;
+console.log(das);
