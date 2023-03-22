4f1460fd3d2b262006b51c711a9f354577337123
= <=>
2023-03-22 19:34:40 +0100

    updating js-script

--- 8_javascript/forms/script.js
+++ 8_javascript/forms/script.js
@@ -1,4 +1,8 @@
 const query = new URLSearchParams(location.search);
-console.log(query.get("name"));
+// console.log(query.get("name"));
+const name = query.get("name");
 const color = query.get("color");
 document.body.style.backgroundColor = color;
+
+const submitObj = { name, color };
+console.table(submitObj);
